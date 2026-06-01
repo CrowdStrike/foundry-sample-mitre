b5203b6d4e27061be688a1c0e5c02da40be13e7d
Matt Raible <matt.raible@crowdstrike.com>
2026-06-01 09:56:47 -0600

    Upgrade @crowdstrike/foundry-js to 0.22.0, vitest to 4.1.8
    Fixes CVE in vitest < 4.1.0 (arbitrary file read/execution). Also removes deprecated transformMode option from vitest configs.

--- ui/extensions/remediations/vitest.config.ts
+++ ui/extensions/remediations/vitest.config.ts
@@ -10,9 +10,6 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      },
       passWithNoTests: true
     }
   })
